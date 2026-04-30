afeafd592f2c35f89235fb64e4af0863ee1ce732
irismolva <irismolvaa@gmail.com>
2026-04-30 21:14:04 +0300

    updated radius values

--- src/PoliceStation.java
+++ src/PoliceStation.java
@@ -3,7 +3,7 @@ public class PoliceStation extends Service{
     public PoliceStation(int n,int m) {
         super(n, m);
         this.symbol ='F';//Force=Police
-        this.radius =4;
+        this.radius =5;
         }
 
 }
--- src/School.java
+++ src/School.java
@@ -2,6 +2,6 @@ public class School extends Service {
     public School(int n, int m) {
         super(n, m);
         this.symbol ='S';
-        this.radius=5;
+        this.radius=4;
     }
 }
