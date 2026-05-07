d88d6489236e88a87823fbbb41435735d36d941c
OzgunEgeDilsiz <ozgunege625@gmail.com>
2026-05-07 16:00:27 +0300

    Update Housing.java
    Level methods for Housing class are complete.

--- src/Housing.java
+++ src/Housing.java
@@ -4,4 +4,37 @@ public class Housing extends Zone {
         super(n,m);
         this.symbol ='H';
     }
+    @Override
+    public void levelUp() {
+        if (level == 0 && getHasElectricity() && getHasWater() && getHasInternet()) {
+            level = 1;
+            System.out.println(getBuildingName() + " at (" + getRow() + "," + getColumn() + ") levels up from 0 to 1");
+        }
+        else if (level == 1 && getHasElectricity() && getHasWater() && getHasInternet() && getHasSecurity() && getHasHealth() && getHasEducation()) {
+            level = 2;
+            System.out.println(getBuildingName() + " at (" + getRow() + "," + getColumn() + ") levels up from 1 to 2");
+        }
+        else if (level == 2 && getHasElectricity() && getHasWater() && getHasInternet() && getHasSecurity() && getHasHealth() && getHasEducation() && getLifestyle() > 0) {
+            level = 3;
+            System.out.println(getBuildingName() + " at (" + getRow() + "," + getColumn() + ") levels up from 2 to 3");
+        }
+    }
+    @Override
+    public void levelDown() {
+        if (!(getHasElectricity() && getHasInternet() && getHasWater())) {
+            if (level != 0) {
+                System.out.println(getBuildingName() + " at (" + getRow() + "," + getColumn() + ") levels down from " + level + " to 0");
+                level = 0;
+            }
+            return;
+        }
+        if (level == 3 && !(getHasSecurity() && getHasHealth() && getHasEducation() && getLifestyle() > 0)) {
+            level = 2;
+            System.out.println(getBuildingName() + " at (" + getRow() + "," + getColumn() + ") levels down from 3 to 2");
+        }
+        else if (level == 2 && !(getHasSecurity() && getHasHealth() && getHasEducation())) {
+            level = 1;
+            System.out.println(getBuildingName() + " at (" + getRow() + "," + getColumn() + ") levels down from 2 to 1");
+        }
+    }
 }
