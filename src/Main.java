8480892e47ec7e8e8c61cf5d2d3d53815005b66a
irismolva <irismolvaa@gmail.com>
2026-05-25 11:31:40 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,49 +10,49 @@ public class Main {
 
     public static void main(String[] args) {
 
-        if(args.length != 2){
+        if(args.length!=2){
             throw new SE116ConfigurationException("ERROR: Missing map file or tick count.");
         }
-        String fileName = args[0];
+        String fileName=args[0];
         int tickCount;
 
         try{
             tickCount = Integer.parseInt(args[1]);
-            if(tickCount < 1){
+            if(tickCount<1){
                 throw new SE116ConfigurationException("ERROR: Tick count must be greater than 0.");
             }
 
-        } catch (NumberFormatException e){
+        } catch(NumberFormatException e){
             throw new SE116ConfigurationException("ERROR: Invalid tick count.", e);
         }
 
-        CityReader cityReader = new CityReader(fileName);
+        CityReader cityReader= new CityReader(fileName);
         cityReader.readAndParseMap();
 
-        Cell[][] cityMap = cityReader.getMapGrid();
+        Cell[][] cityMap= cityReader.getMapGrid();
 
-        Algorithm1 algorithm1 = new Algorithm1();
-        Algorithm2 algorithm2 = new Algorithm2();
-        Algorithm3 algorithm3 = new Algorithm3();
+        Algorithm1 algorithm1 =new Algorithm1();
+        Algorithm2 algorithm2 =new Algorithm2();
+        Algorithm3 algorithm3 =new Algorithm3();
 
-        int totalPopulation = 0;
-        int totalGoods = 0;
-        int totalLifestyle = 0;
+        int totalPopulation=0;
+        int totalGoods=0;
+        int totalLifestyle=0;
 
-        int rows = cityMap.length;
-        int cols = cityMap[0].length;
+        int rows= cityMap.length;
+        int cols= cityMap[0].length;
 
         ArrayList<Zone> allZones = new ArrayList<>();
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
+        for(int i = 0; i<rows; i++){
+            for(int j = 0; j<cols; j++){
                 if(cityMap[i][j] instanceof Zone){
-                    allZones.add((Zone) cityMap[i][j]);
+                    allZones.add((Zone)cityMap[i][j]);
                 }
             }
         }
 
 
-        for(int tick = 1; tick <= tickCount; tick++){
+        for(int tick = 1; tick<=tickCount; tick++){
            System.out.println("TICK " + tick);
 
             algorithm1.provideServices(cityMap);
@@ -63,18 +63,18 @@ public class Main {
             totalGoods = 0;
             totalLifestyle = 0;
 
-            for(Zone zone : allZones){
+            for(Zone zone: allZones){
                 zone.controlOutput();
                 if(zone instanceof Housing){
-                    totalPopulation += zone.getOutput();
+                    totalPopulation+= zone.getOutput();
                 } else if(zone instanceof Industrial){
-                    totalGoods += zone.getOutput();
+                    totalGoods+= zone.getOutput();
                 } else if(zone instanceof Commercial){
-                    totalLifestyle += zone.getOutput();
+                    totalLifestyle+= zone.getOutput();
                 }
             }
             algorithm3.distributeResources(cityMap, totalPopulation, totalGoods, totalLifestyle);
-            allZones.forEach(zone -> zone.resetForNextTick());
+            allZones.forEach(zone-> zone.resetForNextTick());
             
             System.out.println();
         }
