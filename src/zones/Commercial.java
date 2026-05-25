5d5291396248924cd36d7db769ec7343fd2dcd92
OzgunEgeDilsiz <ozgunege625@gmail.com>
2026-05-25 17:59:59 +0300

    Update Commercial.java

--- src/zones/Commercial.java
+++ src/zones/Commercial.java
@@ -7,17 +7,20 @@ public class Commercial extends Zone {
         this.symbol = 'C';
     }
 
-    @Override
+   @Override
     public void levelUp() {
         if (level == 0 && getHasElectricity() && getHasWater() && getHasInternet() && getPopulation() > 0 && getGoods() > 0) {
+            int oldLevel=level;
             level = 1;
-            System.out.println(getBuildingName() + " at (" + getRow() + "," + getColumn() + ") levels up from 0 to 1");
+            displayLevelChange(oldLevel,level);
         } else if (level == 1 && getHasElectricity() && getHasWater() && getHasInternet() && getPopulation() > 0 && getGoods() > 0 && getHasSecurity()) {
+            int oldLevel=level;
             level = 2;
-            System.out.println(getBuildingName() + " at (" + getRow() + "," + getColumn() + ") levels up from 1 to 2");
+            displayLevelChange(oldLevel,level);
         } else if (level == 2 && getHasElectricity() && getHasWater() && getHasInternet() && getPopulation() > 0 && getGoods() > 0 && getHasSecurity()) {
+            int oldLevel=level;
             level = 3;
-            System.out.println(getBuildingName() + " at (" + getRow() + "," + getColumn() + ") levels up from 2 to 3");
+            displayLevelChange(oldLevel,level);
         }
     }
 
@@ -25,22 +28,26 @@ public class Commercial extends Zone {
     public void levelDown() {
         if (!(getHasElectricity() && getHasInternet() && getHasWater())) {
             if (level != 0) {
-                System.out.println(getBuildingName() + " at (" + getRow() + "," + getColumn() + ") levels down from " + level + " to 0");
+                int oldLevel=level;
                 level = 0;
+                displayLevelChange(oldLevel,level);
             }
             return;
         }
         if (level == 3 && !(getPopulation() > 0 && getGoods() > 0 && getHasSecurity())) {
+            int oldLevel=level;
             level = 2;
-            System.out.println(getBuildingName() + " at (" + getRow() + "," + getColumn() + ") levels down from 3 to 2");
+            displayLevelChange(oldLevel,level);
         }
         else if (level == 2 && !(getPopulation() > 0 && getGoods() > 0 && getHasSecurity())) {
+            int oldLevel=level;
             level = 1;
-            System.out.println(getBuildingName() + " at (" + getRow() + "," + getColumn() + ") levels down from 2 to 1");
+            displayLevelChange(oldLevel,level);
         }
         else if (level == 1 && !(getPopulation() > 0 && getGoods() > 0)) {
+            int oldLevel=level;
             level = 0;
-            System.out.println(getBuildingName() + " at (" + getRow() + "," + getColumn() + ") levels down from 1 to 0");
+            displayLevelChange(oldLevel,level);
         }
     }
     @Override
