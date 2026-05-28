4a3a9f1bf0a82d512e43900d8ff70c480c829ead
irismolva <irismolvaa@gmail.com>
2026-05-28 13:36:34 +0300

    teamMeta4126_Exception.java

---
+++ src/teamMeta4126_Exception.java
@@ -0,0 +1,11 @@
+package core;
+
+public class teamMeta4126_Exceptionextends RuntimeException{
+    public teamMeta4126_Exception (String message){
+        super(message);
+    }
+
+    public teamMeta4126_Exception (String message, Throwable errorCause){
+        super(message,errorCause);
+    }
+}
